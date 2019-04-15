c8394ff682f4148f8f210564310f9b7a5439bf30
sebaureta <sureta@outlook.es>
2019-04-15 19:59:33 -0400

    added gems for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
+# Routes
 Rails.application.routes.draw do
   devise_for :users
   resources :products
-root to: "products#index"
+  root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
