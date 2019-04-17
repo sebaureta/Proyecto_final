5c5bed1ea7824678caa5daa67ad15cb94074e0e7
sebaureta <sureta@outlook.es>
2019-04-17 19:48:53 -0400

    solving migration problems

--- db/migrate/20190416003700_create_products.rb
+++ db/migrate/20190416003700_create_products.rb
@@ -1,11 +1,14 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table :products do |t|
       t.string :name
       t.text :description
       t.integer :price
 
       t.timestamps
+    end
+    def down
+      drop_table :products
     end
   end
 end
