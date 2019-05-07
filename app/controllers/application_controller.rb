57b0671cff7e7533854730d9065b6df7bd955f9a
sebaureta <sureta@outlook.es>
2019-05-07 00:57:22 -0400

    added active storage and avatar on navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :image])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, :image])
   end
 end
