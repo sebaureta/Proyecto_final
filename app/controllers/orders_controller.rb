540f725b7952eee0d5552472152f1cebfad94d09
sebaureta <sureta@outlook.es>
2019-05-08 00:15:41 -0400

    shopping car + tables & total

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,16 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   def create
+    @product = Product.find(params[:product_id])
+    @order = Order.new(product: @product, user: current_user)
+    if @order.save
+      redirect_to products_path, notice: 'Producto en Orden'
+    else
+      redirect_to products_path, alert: 'Producto no Ingresado'
+    end
+  end
+
+  def index
+    @orders = current_user.orders
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # Routes
 Rails.application.routes.draw do
   get 'orders/create'
+
   resources :products do
     resources :orders, only: :create
   end
+
+  resources :orders, only: :index
+
   root to: 'products#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
