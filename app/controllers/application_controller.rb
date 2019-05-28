ca7385601c6dac23ff093bb3e745c9b29ddd9a96
sebaureta <sureta@outlook.es>
2019-05-28 00:35:52 -0400

    link_to -> image_tag at images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from CanCan::AccessDenied do
+    respond_to do |format|
+      format.html { redirect_to root_path, alert: 'No puedes ingresar a esta página'}
+    end
+  end
+
   def after_sign_in_path_for(resource)
     if resource.admin?
       admins_path
