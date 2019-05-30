0b0c6c727a6a8e33846ffdcc496cfb0dc2a96801
sebaureta <sureta@outlook.es>
2019-05-30 15:24:28 -0400

    added model Category and Product_Category for NN relation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
+# model products
 class Product < ApplicationRecord
+  # pages per filter
   paginates_per 12
   has_many :orders, dependent: :delete_all
   has_many :users, through: :orders
+
+  has_many :product_categories
+  has_many :categories, through: :product_categories
+
   has_one_attached :image
 end
