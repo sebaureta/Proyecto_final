fb582849a4d71fc2d8a4d5a3ddb2701cf33fe440
sebaureta <sureta@outlook.es>
2019-06-14 09:40:24 -0400

    fixed fonts por heroku

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,3 @@
-config.assets.paths << Rails.root.join("app","assets","fonts")
 require_relative 'boot'
 
 require 'rails/all'
@@ -11,6 +10,7 @@ module Mitiendaonline
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.paths << Rails.root.join("app","assets","fonts")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
